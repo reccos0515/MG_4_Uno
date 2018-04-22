b545af0556af17e9ee9aac67f146078b47e0eea0
Alexander Schulz <ajschulz@iastate.edu>
2018-04-21 21:21:37 -0500

    Implement Lobby v2.0, UNO calls, and other changes

--- Demo2/app/src/main/java/com/example/administrator/demo1/LobbyActivity.java
+++ Demo2/app/src/main/java/com/example/administrator/demo1/LobbyActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +25,7 @@ public class LobbyActivity extends AppCompatActivity {
     private io.socket.client.Socket gsocket=null;
     private String username;
     private ArrayList<String> users = new ArrayList<>();
+    private ArrayList<Integer> usersReady = new ArrayList<>();
     UnoApplication app;
     public String TAG = "LobbyActivity";
     private boolean isConnected;
@@ -42,7 +44,9 @@ public class LobbyActivity extends AppCompatActivity {
         gsocket.on("disconnect",onDisconnect);
         gsocket.on("existed users",onExistedUsers);
         gsocket.on("multiplayer",onStartMultiplayer);
+        gsocket.on("set ready",onExistedUsers);
         gsocket.connect();
+        Log.d("Test",username);
         gsocket.emit("add user",username);
 
     }
@@ -50,16 +54,22 @@ public class LobbyActivity extends AppCompatActivity {
     //Create onClick listener method
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.multiplayer:
-                if(users.size()>1) {
-                    gsocket.emit("multiplayer");
-                }else if(users.size()<2){
-                    Toast.makeText(getApplicationContext(),"Waiting for another user...",Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(getApplicationContext(),"Room is full, Try again later...",Toast.LENGTH_LONG).show();
+            case R.id.toggleGame:
+                //For the host
+                if(users.get(0).equals(username)) {
+                    boolean allReady = checkReady();
+                    if(users.size()>1 && allReady) {
+                        gsocket.emit("multiplayer");
+                    }else if(users.size()<2){
+                        Toast.makeText(getApplicationContext(),"Waiting for another user...",Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Waiting for players to ready up...",Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    gsocket.emit("set ready", username);
                 }
                 break;
-            case R.id.finish:
+            case R.id.exitMatch:
                finish();
                 break;
             default:
@@ -68,22 +78,56 @@ public class LobbyActivity extends AppCompatActivity {
     }
 
     /**
-     * Updates the UI of users in the lobby
-     * @param arr ArrayList of usernames
+     * Decides whether or not all the players are ready to begin the online match
+     * @return True if they are all ready, false if not
      */
-    public void updateUser(ArrayList<String> arr){
-        if(arr!=null) {
-            Log.d("Test",Integer.toString(arr.size()));
+    public boolean checkReady() {
+        for(int ready: usersReady) {
+            if(ready==0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Updates the UI of users in the lobby
+     */
+    public void updateUser(){
+        if(users!=null) {
             LinearLayout llp = findViewById(R.id.playerScroll);
             llp.removeAllViews();
             LinearLayout llnew = new LinearLayout(this);
             llnew.setOrientation(LinearLayout.VERTICAL);
             for (int i = 0; i < users.size(); i++) {
+                LinearLayout llcnew = new LinearLayout(this);
+                llcnew.setOrientation(LinearLayout.HORIZONTAL);
+                ImageView iv = new ImageView(llnew.getContext());
+                TextView tv2 = findViewById(R.id.toggleGame);
+                //Change status bar at the bottom if needed
+                if(users.indexOf(username)==0) {
+                    tv2.setText("Start Game");
+                } else {
+                    tv2.setText("Ready");
+                }
+                if(i==0) {
+                    iv.setImageResource(getResources().getIdentifier("host","drawable",getPackageName()));
+                    Log.d("Test","This is the host!");
+                } else {
+                    Log.d("Test","This is not the host"+"   Ready status: "+usersReady.toString());
+                    if(usersReady.get(i) == 0) {
+                        iv.setImageResource(getResources().getIdentifier("not_ready","drawable",getPackageName()));
+                    } else {
+                        iv.setImageResource(getResources().getIdentifier("ready","drawable",getPackageName()));
+                    }
+                }
                 String user = users.get(i);
                 TextView tv = new TextView(llnew.getContext());
                 tv.setTextSize(30);
                 tv.setText(user);
-                llnew.addView(tv);
+                llcnew.addView(iv);
+                llcnew.addView(tv);
+                llnew.addView(llcnew);
             }
             llp.addView(llnew);
         }
@@ -148,12 +192,16 @@ public class LobbyActivity extends AppCompatActivity {
         @Override
         public void call(final Object... args) {
             users.clear();
+            usersReady.clear();
             JSONArray jsarr = (JSONArray) args[0];
+            JSONArray jsarr2 = (JSONArray) args[1];
             for (int i = 0; i < jsarr.length(); i++) {
                 try {
                     String obj = jsarr.get(i).toString();
                     String name = obj;
+                    int readyStatus = jsarr2.getInt(i);
                     users.add(name);
+                    usersReady.add(readyStatus);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -161,7 +209,7 @@ public class LobbyActivity extends AppCompatActivity {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    updateUser(users);
+                    updateUser();
                 }
             });
         }
